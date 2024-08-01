6e86856367f8a241ec75a7dad04b3892e1de1010
diamond <thisismattdiamond@gmail.com>
2024-08-02 01:40:04 +0200

    Remove config.py and TODO.py from the repository

--- TODO.py
+++
@@ -1,9 +0,0 @@
-# Linkedin auto login - CUT due to 2FA 
-# Manual login - working 
-TODO:  #Chrome needs to save cridentials so you don't need to keep logging in               LOW PRIO
-TODO: #Now needs to engage Selenium automation again                                        HIGH PRIO
-TODO: #Selenium needs to search for jobs - new job.py and logic need to be created or look at the scrape logic in LinkedIn.py          
-    
-    
-    
-    !!!!!SIMPLIFY TO ACHIVE THE GOAL!!!!
